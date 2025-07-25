b6ca8853a1e4d89314020029366a1cbd81c285f7
mmdov <mohammadvary@gmail.com>
2025-07-25 22:27:44 +0330

    added task logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
 use std::env;
+use std::fs::{File, OpenOptions};
 use std::io;
+use std::io::Error;
+use std::io::Write;
 use std::io::prelude::*;
 use std::time;
+
+#[derive(Serialize, Deserialize)]
+struct Task {
+    description: String,
+    time_spent: u64,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +23,19 @@ fn main() {
     pause(&descripion);
 
     let time_spent = now.elapsed().as_secs();
-    let time_spent_minutes = time_spent / 60;
-    let time_spent_seconds = time_spent - (time_spent_minutes * 60);
+
+    let task = Task {
+        description: descripion.to_owned(),
+        time_spent: time_spent.to_owned(),
+    };
+
+    match save_to_json(&task) {
+        Ok(_) => (),
+        Err(_) => println!("Error trying to save to json"),
+    }
+
+    let time_spent_minutes = task.time_spent / 60;
+    let time_spent_seconds = task.time_spent - (time_spent_minutes * 60);
     println!(
         "Done: {descripion}\nDuration: {time_spent_minutes} minutes and {time_spent_seconds} seconds"
     );
@@ -26,4 +49,26 @@ fn pause(description: &str) {
     stdout.flush().unwrap();
 
     let _ = stdin.read(&mut [0u8]).unwrap();
+}
+
+fn save_to_json(task: &Task) -> Result<(), Error> {
+    let json_file = serde_json::json!(&task);
+    let mut tasks: HashMap<String, Value> = if let Ok(mut file) = File::open("tasks.json") {
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        serde_json::from_str(&contents).unwrap_or_default()
+    } else {
+        HashMap::new()
+    };
+
+    tasks.insert(task.description.to_string(), json_file);
+
+    let json_string = serde_json::to_string_pretty(&tasks);
+    let file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("tasks.json");
+    let result = file?.write_all(json_string?.as_bytes());
+    result
 }
